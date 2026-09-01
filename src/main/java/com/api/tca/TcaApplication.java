3aee7fa032e6efcbcfffc9d4ca87b2f3568e6620
Scacchetti <luiscacchetti07@gmail.com>
2026-09-01 17:48:50 -0300

    init: Iniciando API Spring Boot

---
+++ src/main/java/com/api/tca/TcaApplication.java
@@ -0,0 +1,13 @@
+package com.api.tca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/tca/TcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.tca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
